c3f43ff71b371606f3d3d96740a8b00d927758f7
Carrie Schmid <carriepederson7@gmail.com>
2020-08-04 08:43:12 -0700

    Revise timeline.js and add files

---
+++ data/siteConfig.js
@@ -0,0 +1,100 @@
+module.exports = {
+  siteTitle: 'Carrie Schmid',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Carrie Schmid',
+  twitterUsername: '_franciscodf',
+  githubUsername: 'carrieschmid',
+  authorAvatar: '/images/carrie.jpg',
+  authorDescription: `My goal as a software developer is to create high-quality products that are widely understood. I want to help others to better control and organize data. Through this work, I want to build relationships in the digital and non-digital world that lead to new possibilities. 
+  
+ For the last 10 years, I've been working in non-profits in a coordinator role as a planner, administrator and teacher. The signature of my work has been competency, consistency and quality. With these principles in mind, I look forward to working as part of a team to continue to grow products and programs.`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 70,
+    },
+    {
+      name: 'CSS',
+      level: 60,
+    },
+    {
+      name: 'Javascript',
+      level: 50,
+    },
+    {
+      name: 'NodeJs',
+      level: 40,
+    },
+    {
+      name: 'React',
+      level: 60,
+    },
+    {
+      name: 'Git',
+      level: 70,
+    },
+  ],
+  jobs: [
+    {
+      company: 'Zibhub',
+      begin: {
+        month: 'July',
+        year: '2020',
+      },
+      duration: null,
+      occupation: 'Software Application Developer',
+      description:
+        'I am currently working with ZibaHub to create an app to promote workers in the beauty industry. I am working with a team of about 15 people to develop new features in the app and fix bugs. We have a weekly sprint and work in Aglie methodology.'
+    },
+    {
+      company: 'Kamino',
+      begin: {
+        month: 'Feb',
+        year: '2020',
+      },
+      duration: null,
+      occupation: 'Software Engineer Intern',
+      description:
+        'I worked as part of a team of five developers to build and app for in home healthcare for Kamino. We used React Native for the front end, and AWS cloud platform services for the backend. Kamino was in the beginning stages of this project, so I was part of the conversation about app design and data modeling. We worked in an Agile methodology on a JIRA board.',
+    },
+  ],
+
+  educations: [
+    {
+      company: 'Epicodus',
+      begin: {
+        month: 'Apr',
+        year: '2019',
+      },
+      duration: '9 mos',
+      occupation: 'Student',
+      description:
+        'In this six-month at Epicodus,  I studied JavaScript, C# and React. Each day we complete a project using these language, integrating HTML and CSS. Weekly, we complete an independent project.  I passed 20 code reviews, and about six job prep reviews to finish the course. On average, I spent about 8 hours a day working on code.  ',
+    },
+  
+
+    /* ... */
+  ],
+
+  social: {
+    // twitter: 'https://twitter.com/_franciscodf',
+    linkedin: 'https://www.linkedin.com/in/carrie-schmid/',
+    github: 'https://github.com/carrieschmid',
+    email: 'carriepederson7@gmail.com',
+  },
+  siteUrl: 'https://carrieschmid.github.io/resume',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  // googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Carrie Schmid',
+      url: '/',
+    },
+  ],
+}
